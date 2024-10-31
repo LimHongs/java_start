5b6b849f0fab5b2fc7d7c387fbe414fe255e5fef
LimHongs <136678875+LimHongs@users.noreply.github.com>
2024-11-01 00:20:11 +0900

    Overriding

--- java.start/extends1/overriding/GasCar.java
+++ java.start/extends1/overriding/GasCar.java
@@ -1,11 +1,9 @@
 package extends1.overriding;
 
 public class GasCar extends Car {
-	public void move() {
-		System.out.println("차를 이동합니다.");
-	}
 
-	public void fileUp() {
-		System.out.println("기름을 주유합니다");
-	}
+    public void fillUp() {
+        System.out.println("기름을 주유합니다.");
+    }
+
 }
